656ab7524b2a26c1f972b2de82d2db56b3670df2
Wolf <wolfcribbs8@gmail.com>
2025-10-09 04:19:00 -0600

    added header and footer to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="home-page">
+        <header className="site-header">
+            <div className="container">
+            <h1 className="pmg-title">Pursue Marital Goals</h1>
+            <p className="pmg-subtitle">Through Dating</p>
+                <nav>
+                    <menu className="nav">
+                    <li className="nav-item"><a classNaem="nav-link text-light" href="index.html">Home</a></li>
+                    <li className="nav-item"><a className="nav-link text-light" href="key_indicators.html">Key Indicators</a></li>
+                    <li className="nav-item"><a className="nav-link text-light" href="friends.html">Friends</a></li>
+                    <li className="nav-item"><a className="nav-link text-light" href="about.html">About</a></li>
+                    <li className="nav-item"><a className="nav-link text-light" href="contact.html">Contact</a></li>
+                    </menu>
+                </nav>
+            </div>
+        </header>
+
+        <main>App components go here</main>
+
+        <footer className="site-footer">
+          <div className="container">
+            <span className="text-reset">Wolf Cribbs</span>
+            <span>&copy; 2025 PMGDating. All rights reserved.</span>
+            <a href="https://github.com/wcrib8/startup.git" className="footer-link"> GitHub</a>
+          </div>
+        </footer>
+    </div>
+  );
 }
