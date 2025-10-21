bb448cfeddaf1e7c9a3e0cb032e91d0031c5c7d1
Wolf <wolfcribbs8@gmail.com>
2025-10-20 18:20:02 -0600

    edit pics in jsx

--- src/key_indicators/key_indicators.jsx
+++ src/key_indicators/key_indicators.jsx
@@ -6,32 +6,32 @@ export function Key_indicators() {
     <main className="container">
         <table className="indicators-table">
             <tr>
-                <td><img className="rounded" width="80px" src="NewContact.webp" alt="New Contact"/></td>
+                <td><img className="rounded" width="80px" src="new_contact_w.png" alt="New Contact"/></td>
                 <td>New Contact</td>
                 <td>32</td>
             </tr>
             <tr>
-                <td><img className="rounded" width="80px" src="MeaningfulConvo.jpeg" alt="Meaningful Conversation"/></td>
+                <td><img className="rounded" width="80px" src="meaningful_convo_w.png" alt="Meaningful Conversation"/></td>
                 <td>Meaningful Conversation</td>
                 <td>15</td>
             </tr>
             <tr>
-                <td><img className="rounded" width="80px" src="Date.jpeg" alt="Date"/></td>
+                <td><img className="rounded" width="80px" src="date_w.png" alt="Date"/></td>
                 <td>Date</td>
                 <td>8</td>
             </tr>
             <tr>
-                <td><img className="rounded" width="80px" src="Kiss.webp" alt="Kiss"/></td>
+                <td><img className="rounded" width="80px" src="kiss_w.png" alt="Kiss"/></td>
                 <td>Kiss</td>
                 <td>3</td>
             </tr>
             <tr>
-                <td><img className="rounded" width="80px" src="VulnerableMoment.jpeg" alt="Vulnerable Moment"/></td>
+                <td><img className="rounded" width="80px" src="vulnerable_moment_w.png" alt="Vulnerable Moment"/></td>
                 <td>Vulnerable Moment</td>
                 <td>2</td>
             </tr>
             <tr>
-                <td><img className="rounded" width="80px" src="NewPartner.png" alt="New Partner"/></td>
+                <td><img className="rounded" width="80px" src="new_partner_w.png" alt="New Partner"/></td>
                 <td>New Partner</td>
                 <td>1</td>
             </tr>
